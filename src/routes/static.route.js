ad34f3b4bc033d4b2ae8bc96410fa3c718312004
Parin <parin.rau@gmail.com>
2024-03-09 23:55:49 -0800

    add redirects for nonexistent static page urls

--- src/routes/static.route.js
+++ src/routes/static.route.js
@@ -1,26 +1,38 @@
 import express from "express";
 import path from "path";
+import { existsSync } from "fs";
 
 export const staticRouter = express.Router();
 const __dirname = import.meta.dirname;
 
-//staticRouter.use(express.static(path.join(__dirname, "public")));
+staticRouter.use(express.static(path.join(__dirname, "public")));
 
-// staticRouter.get("/:fileName?", (req, res) => {
-// 	const options = {
-// 		root: path.join(__dirname, "..", "public"),
-// 	};
-
-// 	const { fileName } = req.params;
-// 	if (!fileName) return res.redirect("/index.html");
-
-// 	res.sendFile(fileName, options, (err) => {
-// 		if (err) {
-// 			console.error(err.message);
-// 		} else {
-// 			console.log("Sent: ", fileName);
-// 		}
-// 	});
+// staticRouter.get("/config", (req, res) => {
+// 	return res.redirect("/config.html");
 // });
 
-//staticRouter.use("/config");
+staticRouter.get("/:fileName?", (req, res) => {
+	const { fileName } = req.params;
+	const hasHtmlExtension = fileName.slice(-5) === ".html";
+	const fileExists = existsSync(
+		path.join(
+			__dirname,
+			"..",
+			"public",
+			hasHtmlExtension ? fileName : `${fileName}.html`
+		)
+	);
+
+	console.log({ fileName, fileExists });
+
+	if (!hasHtmlExtension && fileExists) {
+		console.log("missing .html extension, but file exists");
+		return res.redirect(`/${fileName}.html`);
+	} else if (!fileName || !fileExists) {
+		console.log("No filename provided or filename doesn't exist");
+		return res.redirect("/index.html");
+	} else {
+		console.log("else fall through");
+		return res.redirect("/index.html");
+	}
+});
